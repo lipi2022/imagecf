d3a63fc27d81a6f42b97a3fd7ba62ff72a819e2c
dialling <lipeng.sh@qq.com>
2022-03-02 11:35:54 +0800

    upload success

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-import { Router } from 'itty-router';
-
-const router = Router()
 const cloudflare_image_url = 'https://api.cloudflare.com/client/v4/accounts/493524dadc6a7fdef16df9d2ad442576/images/v1'
 
-const apiToken = "YUz82BUGJTjFyXOGY22HxtX9atNwJdo2IasOs9XF"
+const apiToken = "sEtZ_f8NV_iLlO1hqcriA6TujIqckOKdAS9SWK4N"
 
-// POST to the collection (we'll use async here)
-router.post('/image', async request => {
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+}
 
+addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (request.method === 'OPTIONS') {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(request));
+  } else if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'POST') {
+    // Handle requests to the API server
+    event.respondWith(handleImage(request));
+  } else {
+    event.respondWith(
+      new Response(null, {
+        status: 405,
+        statusText: 'Method Not Allowed',
+      })
+    );
+  }
+})
+
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  let apiUrl = url.searchParams.get('apiurl');
+
+  if (apiUrl == null) {
+    apiUrl = API_URL;
+  }
+
+  // Rewrite request to point to API URL. This also makes the request mutable
+  // so you can add the correct Origin header to make the API server think
+  // that this request is not cross-site.
+  request = new Request(apiUrl, request);
+  request.headers.set('Origin', new URL(apiUrl).origin);
+  let response = await fetch(request);
+
+  // Recreate the response so you can modify the headers
+  response = new Response(response.body, response);
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', url.origin);
+
+  // Append to/Add Vary header so browser will cache response correctly
+  response.headers.append('Vary', 'Origin');
+
+  return response;
+}
+
+async function handleImage(request) {
   // Prepare the Cloudflare Images API request body
   const formData = await request.formData();
   formData.set("requireSignedURLs", "true");
@@ -30,13 +78,39 @@ router.post('/image', async request => {
   const result = await response.json();
   console.log("upload image result:", result)
 
-  return new Response(result);
-})
+  return new Response(response);
+}
 
-// 404 for everything else
-router.all('*', () => new Response('Not Found.', { status: 404 }))
+function handleOptions(request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers;
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    let respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
+    };
 
-addEventListener('fetch', event => {
-  event.respondWith(router.handle(event.request))
-})
+    return new Response(null, {
+      headers: respHeaders,
+    });
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    });
+  }
+}
 
